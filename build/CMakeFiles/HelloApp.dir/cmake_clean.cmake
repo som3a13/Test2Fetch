a78b7377658bfee360a956930efb02c727e68599
som3a13 <134228578+som3a13@users.noreply.github.com>
2024-04-23 21:03:40 +0200

    build folder

---
+++ build/CMakeFiles/HelloApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloApp.dir/test.c.o"
+  "CMakeFiles/HelloApp.dir/test.c.o.d"
+  "HelloApp"
+  "HelloApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HelloApp.dir/test.c.o.d
@@ -0,0 +1,6 @@
+CMakeFiles/HelloApp.dir/test.c.o: \
+ /home/som3a133/Desktop/GitHub/Test2Fetch/test.c \
+ /home/som3a133/x-tools/arm-cortexa9_neon-linux-musleabihf/arm-cortexa9_neon-linux-musleabihf/sysroot/usr/include/stdc-predef.h \
+ /home/som3a133/x-tools/arm-cortexa9_neon-linux-musleabihf/arm-cortexa9_neon-linux-musleabihf/sysroot/usr/include/stdio.h \
+ /home/som3a133/x-tools/arm-cortexa9_neon-linux-musleabihf/arm-cortexa9_neon-linux-musleabihf/sysroot/usr/include/features.h \
+ /home/som3a133/x-tools/arm-cortexa9_neon-linux-musleabihf/arm-cortexa9_neon-linux-musleabihf/sysroot/usr/include/bits/alltypes.h
